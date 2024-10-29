bc401b51b5febea27d6ec9f3c95145e829ba6a5f
Abhinav <abhinavchawda23@gmail.com>
2024-10-29 23:51:26 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express();
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`)
+})
